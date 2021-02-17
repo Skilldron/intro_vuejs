74443d52b8c7b7b7a04083591b62023420103585
harold delory <harold_delory@ens.univ-artois.fr>
2021-02-17 19:01:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="width : 100%;">
+    <todos></todos>
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
